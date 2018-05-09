f5203d220d1f5f505910e83e70a6e06b881b9ed0
Reuben Ayres <ra@sent.com>
2018-05-09 10:09:15 -0500

    Added one-to-many waiter-orders relation, seeds.sql w seed data, Order model. Ideas about what you might aditionally build.

--- controllers/WaiterController.rb
+++ controllers/WaiterController.rb
@@ -6,9 +6,52 @@ class WaiterController < ApplicationController
   end
 
   # get all the orders for a waiter
+  # which waiter an order goes with
   get '/:id/orders' do
-
+    waiter = Waiter.find_by id: params[:id]
+    this_waiter_orders = waiter.orders
+    {
+      success: true,
+      message: "Found #{this_waiter_orders.length} orders for #{waiter.name}",
+      orders: this_waiter_orders
+    }.to_json
   end
+
+  # you could also use this relation to find:
+  # how many orders is __some waiter__ waiting on (open = true)
+  # how many items were ordered by __some waiter's__ customers today
+  # how many orders have been sent out (open = false) for __some waiter_
+
+  # or you could maybe just a page about each waiter -- 
+  # you'd still ahve to do the same query, but you might
+  # choose to do it in a basic show route like this: 
+
+  # you could use this relation or a similar one or a combination of both
+  # to maybe make a waiter show route?
+  # what info might be useful here?
+  get '/:id' do
+    w = waiter Waiter.find :id
+
+    # you can include whatever you want
+    # what to include will be dictated by client or project manager needs
+
+    {
+      success: true,
+      page_title: "Info for #{w.name}"
+      message: "Route for show page still under construction"
+    }.to_json
+
+  end 
+
+
+
+
+
+  # later: 
+  # get the menu items for a waiter
+  # gets the waiters for a menu item
+
+
 
   post '/' do
 
@@ -31,7 +74,12 @@ class WaiterController < ApplicationController
     @waiter = Waiter.find params[:id]
     @waiter.name = payload[:name]
     @waiter.save
-    @waiter.to_json
+    updated_waiter = @waiter.to_json
+    {
+      success: true,
+      message: "Updated info for waiter no. #{@waiter.id}: #{@waiter.name}",
+      updated_waiter: updated_waiter
+    }.to_json
   end
 
   delete '/:id' do
